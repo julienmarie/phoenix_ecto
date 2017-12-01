0338dffdc69f04b27ae55610ec800641b0520257
Julien Marie <hi@producture.com>
2017-12-01 18:32:08 +0800

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule PhoenixEcto.Mixfile do
 
   defp deps do
     [{:phoenix_html, "~> 2.9", optional: true},
-     {:ecto, "~> 2.1"},
+     {:ecto, "~> 2.1", git: "https://github.com/julienmarie/ecto.git"},
      {:plug, "~> 1.0"},
      {:ex_doc, ">= 0.0.0", only: :dev}]
   end
